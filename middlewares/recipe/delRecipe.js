5db2baf7e6d01d8288cdbe97640480d84f166d66
Koczka Vencel <koczka990@gmail.com>
2022-05-08 17:31:54 +0200

    delete recipe added

--- middlewares/recipe/delRecipe.js
+++ middlewares/recipe/delRecipe.js
@@ -2,8 +2,22 @@
  * deletes existing recipe
  */
 
+ const requireOption = require('../../requireOption');
+
  module.exports = function (objectrepository) {
+
+    const RecipeModel = requireOption(objectrepository, 'RecipeModel');
+
     return function (req, res, next) {
-        return next();
+        if(typeof res.locals.recipe === 'undefined') {
+            return next();
+        }
+        res.locals.recipe.remove((err) => {
+                if(err) {
+                    return next(err);
+                }
+                return res.redirect('/');
+            });
+        
     };
 };
