d863acfd92367098f0ce4831cf81860bf6a1c5dc
Koczka Vencel <koczka990@gmail.com>
2022-05-08 17:35:10 +0200

    getingredients implemented

--- middlewares/ingredient/getIngredients.js
+++ middlewares/ingredient/getIngredients.js
@@ -2,42 +2,21 @@
  * returns list of all existing ingredients
  */
 
+ const requireOption = require('../../requireOption');
+
  module.exports = function (objectrepository) {
+
+    const IngredientModel = requireOption(objectrepository, 'IngredientModel');
+
     return function (req, res, next) {
 
-        res.locals.ingredients = [
-            {
-                _id: 'id1',
-                name: 'Apple',
-                measurement: 'kg',
-                unit: 1,
-                kcal: 520,
-                protein: 3,
-                carbohydrate: 114,
-                fat: 2
-            },
-            {
-                _id: 'id2',
-                name: 'Flour',
-                measurement: 'g',
-                unit: 100,
-                kcal: 337,
-                protein: 9.8,
-                carbohydrate: 71,
-                fat: 1
-            },
-            {
-                _id: 'id3',
-                name: 'Cukor',
-                measurement: 'g',
-                unit: 100,
-                kcal: 387,
-                protein: 0,
-                carbohydrate: 100,
-                fat: 0
+        IngredientModel.find({}, (err, ingredients) => {
+            if(err){
+                return next(err);
             }
-        ]
 
-        return next();
+            res.locals.ingredients = ingredients;
+            return next();
+        });
     };
 };
