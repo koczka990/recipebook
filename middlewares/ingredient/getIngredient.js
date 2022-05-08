0f766635f0e279020371c7770131c485507b90b7
Koczka Vencel <koczka990@gmail.com>
2022-05-08 17:38:27 +0200

    get specific ingredient implemented

--- middlewares/ingredient/getIngredient.js
+++ middlewares/ingredient/getIngredient.js
@@ -2,8 +2,20 @@
  * returns specific ingredient specified by :ingredientid
  */
 
+ const requireOption = require('../../requireOption');
+
  module.exports = function (objectrepository) {
+
+    const IngredientModel = requireOption(objectrepository, 'IngredientModel');
+
     return function (req, res, next) {
-        return next();
+        IngredientModel.findOne({_id: req.params.ingredientid}, 
+            (err, ingredient) => {
+                if(err || !ingredient) {
+                    return next(err);
+                }
+                res.locals.ingredient = ingredient;
+                return next();
+            });
     };
 };
